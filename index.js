b45d6efafad386bc67e514c358a0ab395415434c
LiangYujunyi-2425 <240002257@stu.vtc.edu.hk>
2025-09-08 09:00:17 +0000

    new version

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const server = http.createServer(function(req, res){
-    res.write("project for fyp")
-    res.end
-})
+    res.write("project for fyp");
+    res.end;
+});
+
+server.listen(3000);
