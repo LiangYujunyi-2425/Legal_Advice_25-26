d2e9f22d8e855f1c0cd65f2bbc60421eeaf0dc9d
LiangYujunyi-2425 <240002257@stu.vtc.edu.hk>
2025-09-09 03:48:51 +0000

    new version

---
+++ legal_advice_project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Title from './Title'
+//import './App.css'
+//component名称需要大写
+
+function App() {
+  
+  return (
+    <>
+
+      <div>
+        <Title/>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ legal_advice_project/src/Title.jsx
@@ -0,0 +1,4 @@
+function Title(){
+    return <h2>project画面支持</h2>
+}
+export default Title
